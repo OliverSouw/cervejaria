7ff0f2bef4e8b6614d809f0f82861297919e6ab3
OliverSouw <igor_oliveira94@hotmail.com>
2022-12-03 12:43:12 -0300

    Corrigindo o JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 
-function verificaIade(valor) {
+function verificaIdade(valor) {
     document.getElementById('btn-sim');
 
     if (valor === 1) {
-        window.location.href = 'home.html?valor=1';
+        window.location.href = 'home.html';
     } else {
         window.location.href = 'siteNegado.html';
     }
